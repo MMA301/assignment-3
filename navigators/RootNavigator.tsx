994743178c3399aeac553c3a5b98bc18453892bc
Minh Vương <hardingadonis@gmail.com>
2024-09-09 00:03:17 +0700

    update: finish assignment

--- navigators/RootNavigator.tsx
+++ navigators/RootNavigator.tsx
@@ -7,7 +7,7 @@ const Stack = createNativeStackNavigator();
 
 const RootNavigator = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator initialRouteName="Welcome">
       <Stack.Screen name="Welcome" component={WelcomeScreen} />
       <Stack.Screen name="Subscribe" component={SubscribeScreen} />
     </Stack.Navigator>
